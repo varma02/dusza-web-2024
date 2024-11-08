513ad6f1db5d5b1da0db55af6cefbb06b1dc4c5e
varma02 <varma02.marci@gmail.com>
2024-11-08 12:12:37 +0000

    remove db test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,18 @@
 import { Button } from "@nextui-org/react";
-import prisma from "@/lib/db";
-import { FaChevronRight } from "react-icons/fa";
+// import prisma from "@/lib/db";
+// import { FaChevronRight } from "react-icons/fa";
 
 export default async function Home() {
-  const test = await prisma.test.findMany();
+  // const test = await prisma.test.findMany();
 
   return (
     <div>
       <Button color="primary">Hello, World!</Button>
-      <ul>
+      {/* <ul>
         {test.map((t) => (
           <li key={t.id}><FaChevronRight className="inline"/> {t.text}</li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 }
