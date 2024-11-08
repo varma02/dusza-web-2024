ae4821c26e786c48d2ea104c939ab0fdd8f6f7a6
varma02 <varma02.marci@gmail.com>
2024-11-08 10:27:32 +0000

    add react-icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@nextui-org/react";
 import prisma from "@/lib/db";
+import { FaChevronRight } from "react-icons/fa";
 
 export default async function Home() {
   const test = await prisma.test.findMany();
@@ -9,7 +10,7 @@ export default async function Home() {
       <Button color="primary">Hello, World!</Button>
       <ul>
         {test.map((t) => (
-          <li key={t.id}>{t.text}</li>
+          <li key={t.id}><FaChevronRight className="inline"/> {t.text}</li>
         ))}
       </ul>
     </div>
