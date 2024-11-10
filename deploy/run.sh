c1bf49ecc9c6ba17108a9e2a85a7e6c1205e2cea
varma02 <varma02.marci@gmail.com>
2024-11-10 18:56:13 +0000

    script update

--- deploy/run.sh
+++ deploy/run.sh
@@ -5,7 +5,7 @@ WORKDIR="/home/dusza/dusza-web-2024/"
 cd $WORKDIR
 
 while true; do
-  if ! npm run start then
+  if ! npm run start; then
     echo "Application exited with an error. Retrying in 5 seconds..."
   else
     echo "Application exited. Restarting in 5 seconds..."
